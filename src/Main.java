b3b209e49f6011181fe6b83ba9b024cdff46fa00
Tigradcom <tigradcom2@gmail.com@>
2022-05-22 12:53:54 +0400

    updating values

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
         int eaters = 5; // сколько людей будут есть
 
-        int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
+        int water = 4333; // миллилитров воды
+        int potatoes = 7; // картофелин
+        int chicken = 7; // куриных бёдер
+        int spices = 7; // ложек специй
 
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров воды");
